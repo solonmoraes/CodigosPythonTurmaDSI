0dffa889d0a39c02871426cab24b663327c8b6f9
Solon moraes <solonmoraes2017@gmail.com>
2021-02-01 15:57:35 -0200

    feito as atv 1,2,3 de python sequencial

---
+++ Exercicio1/primeira atividade 1.py
@@ -0,0 +1,13 @@
+
+print("********Atividade*********")
+
+m2 = 850
+base = float(input("Qual a largura da (base) do terreno? "))
+altura = float(input("Qual o comprimento (altura) do terreno?"))
+
+area = base * altura
+
+custo = area * m2
+print("Voce ira pagar um total de R$ {} para contruir.\n\n".format(custo))
+
+
---
+++ Exercicio1/segunda atv.py
@@ -0,0 +1,4 @@
+print("===========Atividade=============")
+salaTotal = float(input("Informe o quanto recebeu:\n"))
+horasDia = int(input("Informe suas horas trabalhadas:\n"))
+print(salaTotal / horasDia)
